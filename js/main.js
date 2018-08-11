d68079d1f3588b3494a35670c7f14d089f865787
marcus delbianco <mdelbianco9@gmail.com>
2018-08-11 10:15:37 -0700

    Added final touches on hover effects. Added purple blur on img hover.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,40 @@
 
 
 
-$(".button-1").hover(function() {
+$(".button-1").mouseenter(function() {
   $(".blur").css("background-color", "");
 }, function() {
-  $(".blur-box").css("background-color", "red");
-})
+  $(".blur-box").css("background-color", "#f5f0ff");
+});
+
+$(".button-1").mouseleave(function() {
+  $(".blur").css("background-color", "");
+}, function() {
+  $(".blur-box").css("background-color", "");
+});
+
+
+
+$(".button-2").mouseenter(function() {
+  $(".blur").css("background-color", "");
+}, function() {
+  $(".blur-box").css("background-color", "#ffc2a4");
+});
+
+$(".button-2").mouseleave(function() {
+  $(".blur").css("background-color", "");
+}, function() {
+  $(".blur-box").css("background-color", "");
+});
+
+$("img").mouseenter(function() {
+  $(".blur").css("background-color", "");
+}, function() {
+  $(".blur-box").css("background-color", "#400080");
+});
+
+$("img").mouseleave(function() {
+  $(".blur").css("background-color", "");
+}, function() {
+  $(".blur-box").css("background-color", "");
+});
